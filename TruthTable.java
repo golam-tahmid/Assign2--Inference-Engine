d969d72deb6478ed9ce99687f6eb03ec598af9c2
erikfanning <132543883+erikfanning@users.noreply.github.com>
2023-05-19 23:29:39 +1000

    Update TruthTable.java

--- TruthTable.java
+++ TruthTable.java
@@ -4,6 +4,7 @@ public class TruthTable {
     private List<Clause> knowledgeBase;
     private Literal query;
     private Set<String> symbols;
+    private int modelsEntailment;
 
     public TruthTable(List<Clause> knowledgeBase, Literal query) {
         this.knowledgeBase = knowledgeBase;
@@ -25,18 +26,26 @@ public class TruthTable {
         List<Map<String, Boolean>> models = generateModels(new ArrayList<>(symbols));
         boolean isEntailed = false;
 
+        // Counters for the number of models where the KB and query are true
+        int modelsKB = 0;
+
         // Check each model
         for (Map<String, Boolean> model : models) {
             if (evaluateModel(model)) {
+                // If the knowledge base is true, increment the modelsKB counter
+                modelsKB++;
                 // If the query is true in a model where the knowledge base is also true,
                 // then the query is entailed by the knowledge base.
                 if (model.getOrDefault(query.symbol, false) == query.isPositive) {
                     isEntailed = true;
+                    modelsEntailment++;
                 }
             }
             System.out.println("Evaluation of model " + model + ": " + (evaluateModel(model) ? "true" : "false"));
         }
-
+        // Print the number of models where the KB and query are true
+        System.out.println("Number of models where KB is true: " + modelsKB);
+        System.out.println("Number of models where KB entails the query: " + modelsEntailment);
         // Return whether the query is entailed by the knowledge base
         return isEntailed;
     }
@@ -73,26 +82,38 @@ public class TruthTable {
         return models;
     }
 
+    public int getModelsEntailment() {
+        return this.modelsEntailment;
+    }
 
     private boolean evaluateModel(Map<String, Boolean> model) {
         for (Clause clause : knowledgeBase) {
+            // Assume the clause is false until proven otherwise
             boolean clauseIsTrue = false;
+
             for (Literal literal : clause.literals) {
-                Boolean value = model.get(literal.symbol);
-                if (value != null && value == literal.isPositive) {
-                    // This literal is true, so the clause is true
+                Boolean valueInModel = model.get(literal.symbol);
+
+                // Determine the truth value of the literal in the current model
+                boolean literalIsTrue = literal.isPositive ? valueInModel : !valueInModel;
+
+                // If the literal is true, the entire clause is true (since we're dealing with disjunctions)
+                if (literalIsTrue) {
                     clauseIsTrue = true;
-                    break;
+                    break; // No need to check the rest of the literals in this clause
                 }
             }
+
+            // If none of the literals in the clause are true, the clause is false
             if (!clauseIsTrue) {
-                // This clause is false, so the knowledge base is false
-                return false;
+                return false; // This clause is false, so the knowledge base is false
             }
         }
 
         // All clauses are true, so the knowledge base is true
         return true;
     }
+
+
 
 }
